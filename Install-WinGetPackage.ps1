02d4e62cfe793b05acde79d0c25a3fed87112131
Jay Price <price.jamie@gmail.com>
2022-07-30 18:55:33 +0100

    Correction

--- Install-WinGetPackage.ps1
+++ Install-WinGetPackage.ps1
@@ -46,10 +46,7 @@ param(
     [parameter(Mandatory = $false, Position = 2)]
     [switch]$AcceptAgreements, 
 
-<<<<<<< HEAD
     # Scope of Installation
-=======
->>>>>>> main
     [parameter(Mandatory = $false, HelpMessage = "Select if User or Machine Installation Scope", Position = 3)]
     [ValidateSet('User', 'Machine')]
     [string]$Scope = "User"
@@ -61,10 +58,7 @@ try {
     if(!$AcceptAgreements) {
         Write-Verbose -Message "Install WinGet Package $($PackageId)"
         
-<<<<<<< HEAD
         # Is this going to be Machine Scope Install
-=======
->>>>>>> main
         if($scope -eq "Machine" ) {
             Write-Verbose -Message "Install $($PackageId) as Machine"
             Start-Process -FilePath "WinGet" -ArgumentList "install -e --id $($PackageId) --scope Machine" -ErrorAction Stop -Wait -NoNewWindow
@@ -75,11 +69,8 @@ try {
         }
     } else {
         Write-Verbose -Message "Install WinGet Package $($PackageId) with Package Agreement Selected"
-<<<<<<< HEAD
-        # Is this going to be Machine Scope Install
-=======
 
->>>>>>> main
+        # Is this going to be Machine Scope Install
         if($scope -eq "Machine" ) {        
             Write-Verbose -Message "Install $($PackageId) as Machine"
             Start-Process -FilePath "WinGet" -ArgumentList "install --id $($PackageId) --accept-package-agreements --scope Machine " -ErrorAction Stop -Wait -NoNewWindow
